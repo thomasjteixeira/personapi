e1b02c596578e80b3674cbd5c54c07412a97e2e6
Thomas Jefferson <thomas@debian-BULLSEYE-live-builder-AMD64>
2022-01-30 18:36:24 -0300

    Developed unit tests for create person

---
+++ src/test/java/one/digitalinnovation/personapi/service/PersonServiceTest.java
@@ -0,0 +1,44 @@
+package one.digitalinnovation.personapi.service;
+
+import one.digitalinnovation.personapi.dto.MessageResponseDTO;
+import one.digitalinnovation.personapi.dto.request.PersonDTO;
+import one.digitalinnovation.personapi.entity.Person;
+import one.digitalinnovation.personapi.repository.PersonRepository;
+import one.digitalinnovation.personapi.utils.PersonUtils;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static one.digitalinnovation.personapi.utils.PersonUtils.createFakeEntity;
+import static org.mockito.Mockito.any;
+
+@ExtendWith(MockitoExtension.class)
+public class PersonServiceTest {
+
+    @Mock
+    private PersonRepository personRepository;
+
+    @InjectMocks
+    private PersonService personService;
+
+    @Test
+    void testGivenPersonDTOThenReturnSavedMessage() {
+        PersonDTO personDTO = PersonUtils.createFakeDTO();
+        Person expectedSavedPerson = createFakeEntity();
+
+        Mockito.when(personRepository.save(any(Person.class))).thenReturn(expectedSavedPerson);
+
+        MessageResponseDTO expectedSuccessMessage = MessageResponseDTO
+                .builder()
+                .message("Create person with ID " + expectedSavedPerson.getId())
+                .build();
+
+        MessageResponseDTO successMessage = personService.createPerson(personDTO);
+
+        Assertions.assertEquals(expectedSuccessMessage, successMessage);
+    }
+}
